2297160742117cd6a6c49e300faf29dfa92a549a
LauralyMontuelle <144046040+LauralyMontuelle@users.noreply.github.com>
2023-09-25 10:38:22 +0200

    test

--- test.py
+++ test.py
@@ -2,43 +2,153 @@ import pandas
 import folium
 import streamlit as st
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 st.title("Etude des accidents de vélos depuis 2005")
 st.write("Bienvenue !")
 
 # Utilisez pd.read_excel() pour lire le fichier Excel dans un DataFrame
-data = pandas.read_excel(r"accidentsVelo.xlsx", decimal=",")  
-data.head(5)
+@st.cache
+def import_data():
+    data = pandas.read_excel(r"accidentsVelo.xlsx", decimal=",")  
+    data = data.loc[data['an'] >= 2015]
+    # Créer une correspondance département - région
+    departement_region = {
+        1: 'Auvergne-Rhône-Alpes',
+        2: 'Hauts-de-France',
+        3: 'Auvergne-Rhône-Alpes',
+        4: 'Provence-Alpes-Côte d\'Azur',
+        5: 'Provence-Alpes-Côte d\'Azur',
+        6: 'Provence-Alpes-Côte d\'Azur',
+        7: 'Auvergne-Rhône-Alpes',
+        8: 'Grand Est',
+        9: 'Occitanie',
+        10: 'Grand Est',
+        11: 'Occitanie',
+        12: 'Occitanie',
+        13: 'Provence-Alpes-Côte d\'Azur',
+        14: 'Normandie',
+        15: 'Auvergne-Rhône-Alpes',
+        16: 'Nouvelle-Aquitaine',
+        17: 'Nouvelle-Aquitaine',
+        18: 'Centre-Val de Loire',
+        19: 'Nouvelle-Aquitaine',
+        21: 'Bourgogne-Franche-Comté',
+        22: 'Bretagne',
+        23: 'Nouvelle-Aquitaine',
+        24: 'Nouvelle-Aquitaine',
+        25: 'Bourgogne-Franche-Comté',
+        26: 'Auvergne-Rhône-Alpes',
+        27: 'Normandie',
+        28: 'Centre-Val de Loire',
+        29: 'Bretagne',
+        '2A': 'Corse',
+        '2B': 'Corse',
+        30: 'Occitanie',
+        31: 'Occitanie',
+        32: 'Occitanie',
+        33: 'Nouvelle-Aquitaine',
+        34: 'Occitanie',
+        35: 'Bretagne',
+        36: 'Centre-Val de Loire',
+        37: 'Centre-Val de Loire',
+        38: 'Auvergne-Rhône-Alpes',
+        39: 'Bourgogne-Franche-Comté',
+        40: 'Nouvelle-Aquitaine',
+        41: 'Centre-Val de Loire',
+        42: 'Auvergne-Rhône-Alpes',
+        43: 'Auvergne-Rhône-Alpes',
+        44: 'Pays de la Loire',
+        45: 'Centre-Val de Loire',
+        46: 'Occitanie',
+        47: 'Nouvelle-Aquitaine',
+        48: 'Occitanie',
+        49: 'Pays de la Loire',
+        50: 'Normandie',
+        51: 'Grand Est',
+        52: 'Grand Est',
+        53: 'Pays de la Loire',
+        54: 'Grand Est',
+        55: 'Grand Est',
+        56: 'Bretagne',
+        57: 'Grand Est',
+        58: 'Bourgogne-Franche-Comté',
+        59: 'Hauts-de-France',
+        60: 'Hauts-de-France',
+        61: 'Normandie',
+        62: 'Hauts-de-France',
+        63: 'Auvergne-Rhône-Alpes',
+        64: 'Nouvelle-Aquitaine',
+        65: 'Occitanie',
+        66: 'Occitanie',
+        67: 'Grand Est',
+        68: 'Grand Est',
+        69: 'Auvergne-Rhône-Alpes',
+        70: 'Bourgogne-Franche-Comté',
+        71: 'Bourgogne-Franche-Comté',
+        72: 'Pays de la Loire',
+        73: 'Auvergne-Rhône-Alpes',
+        74: 'Auvergne-Rhône-Alpes',
+        75: 'Île-de-France',
+        76: 'Normandie',
+        77: 'Île-de-France',
+        78: 'Île-de-France',
+        79: 'Nouvelle-Aquitaine',
+        80: 'Hauts-de-France',
+        81: 'Occitanie',
+        82: 'Occitanie',
+        83: 'Provence-Alpes-Côte d\'Azur',
+        84: 'Provence-Alpes-Côte d\'Azur',
+        85: 'Pays de la Loire',
+        86: 'Nouvelle-Aquitaine',
+        87: 'Nouvelle-Aquitaine',
+        88: 'Grand Est',
+        89: 'Bourgogne-Franche-Comté',
+        90: 'Bourgogne-Franche-Comté',
+        91: 'Île-de-France',
+        92: 'Île-de-France',
+        93: 'Île-de-France',
+        94: 'Île-de-France',
+        95: 'Île-de-France',
+        971: 'Guadeloupe',
+        972: 'Martinique',
+        973: 'Guyane',
+        974: 'La Réunion',
+        976: 'Mayotte'
+    }
+    data['region'] = data['dep'].map(departement_region)
+    return data
 
-# Compter le nombre d'accident par an
-acc_an = data['an'].value_counts().reset_index()
-acc_an.columns = ['annee', 'nombre_accidents']
+data = import_data()
 
-# affichage du nombre d'accident par an
-fig = plt.figure(figsize=(10, 6))
+with st.sidebar:
+        Gender_filter = st.multiselect(label='Selectionnez un genre',
+                            options=data['sexe'].unique(),
+                            default=data['sexe'].unique())
+        region_filter = st.multiselect(label='Selectionnez une région',
+                            options=data['region'].unique(),
+                            default=data['region'].unique())
 
-plt.bar(acc_an['annee'], acc_an['nombre_accidents'])
-plt.xlabel('Année du sinistre')
-plt.ylabel('Nombre d accident de vélo')
-plt.xticks(acc_an['annee'])
-plt.title('Evolution annuelle du nombre d accident de velo en France')
+df_filtre = data.query('region == @region_filter & sexe == @Gender_filter')
 
-st.pyplot(fig)
+Q1,Q2 = st.columns(2,gap='large')
 
-# Créez un curseur pour sélectionner une année
-annee_selectionnee = st.slider("Sélectionnez une année", min_value=acc_an['annee'].min(), max_value=acc_an['annee'].max(), value=acc_an['annee'].max(), step=1)
-
-# Filtrer les données en fonction de l'année sélectionnée
-donnees_filtrees = acc_an[acc_an['annee'] == annee_selectionnee]
-
-# Créez un histogramme des valeurs pour l'année sélectionnée
-fig, ax = plt.subplots()
-ax.bar(donnees_filtrees['annee'], donnees_filtrees['nombre_accidents'])
-ax.set_xlabel('annee')
-ax.set_ylabel('nombre_accidents')
-ax.set_title(f'Histogramme pour l\'année {annee_selectionnee}')
-
-# Affichez le graphique dans Streamlit
-st.pyplot(fig)
-
-
+with Q1:
+    df1 = df_filtre.groupby(by = ['an'])['Num_Acc'].count().reset_index()
+    df1 = df1.rename(columns={'Num_Acc': 'nb_accidents'})
+    fig_CTR_by_campaign = px.bar(df1,
+                            x='an',
+                            y='nb_accidents',
+                            title='Nombre d\'accidents par année')
+    fig_CTR_by_campaign.update_layout(title = {'x' : 0.5},
+                                    plot_bgcolor = "rgba(0,0,0,0)",
+                                    xaxis =(dict(showgrid = False)),
+                                    yaxis =(dict(showgrid = False)))
+    st.plotly_chart(fig_CTR_by_campaign,use_container_width=True)
+st.text("    ")
+with Q2:
+    df2 = df_filtre.groupby(by = ['grav'])['Num_Acc'].count().reset_index()
+    df2 = df2.rename(columns={'Num_Acc': 'nb_accidents'})
+    fig_spend_by_gender = px.pie(df2,names='grav',values='nb_accidents',title='Type d\'accients')
+    fig_spend_by_gender.update_layout(title = {'x':0.5}, plot_bgcolor = "rgba(0,0,0,0)")
+    st.plotly_chart(fig_spend_by_gender,use_container_width=True)
