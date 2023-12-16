ea6847a0b10bc4a62cc0b2a0b82dfb9c215b0c8d
LauralyMontuelle <144046040+LauralyMontuelle@users.noreply.github.com>
2023-12-16 18:59:42 +0100

    derniere

--- Page3_stream.py
+++ Page3_stream.py
@@ -25,6 +25,7 @@ import plotly.express as px
 from sklearn.neighbors import KNeighborsClassifier
 from xgboost import XGBClassifier
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import StratifiedShuffleSplit
 
 # Définissez la largeur de la page Streamlit
 st.set_page_config(layout="wide")
@@ -175,140 +176,153 @@ def import_data():
 
 data = import_data()
 
-# Séparer les features et la variable cible
-X = data[["secuexist", "age", "region2", "lum", "atm", "catr", "trajet", "equipement"]]
-#X = data[["age", "region2", "catr", "trajet"]]
-y = data[["grav"]]
+@st.cache_data
+def test_model():
+    # Séparer les features et la variable cible
+    X = data[["secuexist", "age", "region2", "lum", "atm", "catr", "trajet", "equipement"]]
+    y = data['grav']
 
-# Imputer les valeurs manquantes
-imputer = SimpleImputer(strategy='most_frequent')
-X_imputed = pd.DataFrame(imputer.fit_transform(X), columns=X.columns)
+    # Imputer les valeurs manquantes
+    imputer = SimpleImputer(strategy='most_frequent')
+    X_imputed = pd.DataFrame(imputer.fit_transform(X), columns=X.columns)
 
-# Diviser les données en ensembles d'entraînement et de test
-X_train, X_test, y_train, y_test = train_test_split(X_imputed, y, test_size=0.2, random_state=42)
+    # Diviser les données en ensembles d'entraînement et de test avec stratification
+    X_train, X_test, y_train, y_test = train_test_split(X_imputed, y, test_size=0.2, random_state=42, stratify=y)
+    models = {
+        'Random Forest': RandomForestClassifier(),
+        'KNN': KNeighborsClassifier(),
+        'XGBoost': XGBClassifier(),
+        'SVM': SVC()
+    }
+    # Créer un DataFrame pour stocker les importances de chaque modèle
+    importances_df = pd.DataFrame(index=X_imputed.columns)
+    # Boucle pour entraîner et évaluer chaque modèle
+    results = {}
+    class_accuracies = {}
+    for model_name, model in models.items():
+        model.fit(X_train, y_train.values.ravel())
+        y_pred = model.predict(X_test)
+        # Calculer des métriques de performance 
+        accuracy = accuracy_score(y_test, y_pred)
+        # Extraire la matrice de confusion
+        conf_matrix = confusion_matrix(y_test, y_pred)
+        # Calculer le pourcentage de prédictions correctes par classe
+        class_accuracy = conf_matrix.diagonal() / conf_matrix.sum(axis=1)
+        # Stocker les résultats
+        results[model_name] = {'model': model, 'accuracy': accuracy}
+        class_accuracies[model_name] = {'class_accuracy': class_accuracy}
 
-# Modèles à tester
-models = {
-    'Random Forest': RandomForestClassifier(),
-    'KNN': KNeighborsClassifier(),
-    'XGBoost': XGBClassifier(),
-    'SVM': SVC()
-}
+    # Créer une figure avec 4 sous-plots disposés en ligne
+    fig, axes = plt.subplots(1, 4, figsize=(16, 4))
 
-# Créer un DataFrame pour stocker les importances de chaque modèle
-importances_df = pd.DataFrame(index=X_imputed.columns)
+    # Afficher la matrice de confusion pour chaque modèle
+    for ax, (model_name, model_conf_matrix) in zip(axes, zip(class_accuracies.keys(), [confusion_matrix(y_test, model.predict(X_test)) for model in models.values()])):
+        ax.set_title(f"Matrice de Confusion pour {model_name}")
+        
+        # Calculer les pourcentages
+        total_per_class = model_conf_matrix.sum(axis=1)
+        conf_matrix_percentage = (model_conf_matrix.T / total_per_class).T * 100
 
-# Boucle pour entraîner et évaluer chaque modèle
-results = {}
-for model_name, model in models.items():
-    model.fit(X_train, y_train.values.ravel())
-    y_pred = model.predict(X_test)
-    
-    # Calculer des métriques de performance 
+        # Afficher une heat map de la matrice de confusion
+        sns.heatmap(conf_matrix_percentage, annot=True, fmt='.2f', cmap='Blues', cbar=False, ax=ax)
+        ax.set_xlabel('Valeurs Prédites')
+        ax.set_ylabel('Valeurs Réelles')
+
+    # Ajuster l'espacement entre les sous-plots
+    plt.tight_layout()
+
+    # Afficher la figure
+    st.pyplot(fig)
+
+    # Créer un DataFrame pour les accuracies
+    accuracies_df = pd.DataFrame(results).T  # Transposer pour avoir les modèles en index
+    accuracies_df.reset_index(inplace=True)
+    accuracies_df.columns = ['Model', 'Data']
+    accuracies_df[['Model', 'Accuracy']] = pd.DataFrame(accuracies_df['Data'].tolist(), index=accuracies_df.index)
+    accuracies_df = accuracies_df[['Model', 'Accuracy']]
+
+    # Appliquer une mise en forme conditionnelle pour mettre en couleur la ligne du modèle XGBoost
+    styled_df = accuracies_df.style.apply(lambda x: ['background: lightblue' if x['Model'] == 'XGBoost' else '' for i in x], axis=1)
+
+    # Afficher le DataFrame stylé
+    st.dataframe(styled_df, use_container_width=True)
+
+    st.write("Nous choisissons le modèle XGboost pour notre application.")
+
+    # Entraîner le modèle XGBoost
+    xgb_model = models['XGBoost']
+    xgb_model.fit(X_train, y_train.values.ravel())
+
+    # Obtenir les importances des caractéristiques du modèle XGBoost
+    importances_df['XGBoost'] = xgb_model.feature_importances_
+
+    # Afficher les importances des variables pour le modèle choisi (XGBoost)
+    chosen_model_importances = importances_df['XGBoost']
+    # Afficher un graphique à barres pour les importances des variables du modèle choisi (XGBoost)
+    fig1 = px.bar(x=chosen_model_importances.index, y=chosen_model_importances.values, title='Test')
+    fig1.update_layout(
+        plot_bgcolor="rgba(0,0,0,0)",
+        xaxis=dict(showgrid=False, title='Caractéristiques'),
+        yaxis=dict(showgrid=False, title='Importance')
+    )
+    st.plotly_chart(fig1, use_container_width=True, figsize=(10, 6))
+
+test_model()
+
+st.write("Nous remarquons pour chaque modèle que les classes 0 et 3 ne sont pas bien prédites en raison de leur faible représentation dans la base de données.")
+st.write("Pour vaincre cela, nous allons rassemblé les classes 0 et 1 et les classe 2 et 3.")
+
+@st.cache_data
+def xgboost_merged_classes():
+    # Séparer les features et la variable cible
+    X = data[['age', 'region2', 'catr', 'trajet']]
+    y = data['grav']
+
+    # Fusionner les classes 0 et 1 en une seule classe (classe 0)
+    # Fusionner les classes 2 et 3 en une seule classe (classe 1)
+    y_merged = y.replace({0: 0, 1: 0, 2: 1, 3: 1})
+
+    # Diviser les données en ensembles d'entraînement et de test avec stratification
+    X_train, X_test, y_train, y_test = train_test_split(X, y_merged, test_size=0.2, random_state=42, stratify=y_merged)
+
+    # Définir les hyperparamètres à optimiser
+    param_dist = {
+        'learning_rate': [0.01, 0.1, 0.2],
+        'n_estimators': [50, 100, 200],
+        'max_depth': [3, 5, 7],
+        'subsample': [0.8, 0.9, 1.0],
+        'colsample_bytree': [0.8, 0.9, 1.0],
+        'gamma': [0, 1, 2],
+        'min_child_weight': [1, 2, 3],
+        'lambda': [0, 1, 2],
+        'alpha': [0, 1, 2]
+    }
+
+    # Créer le modèle XGBoost
+    xgb = XGBClassifier()
+
+    # Effectuer une recherche aléatoire des hyperparamètres
+    random_search = RandomizedSearchCV(xgb, param_distributions=param_dist, n_iter=10, scoring='accuracy', cv=3, random_state=42)
+    random_search.fit(X_train, y_train)
+
+    # Afficher les meilleurs hyperparamètres
+    best_params = random_search.best_params_
+    st.write("Meilleurs hyperparamètres :", best_params)
+
+    # Utiliser le modèle avec les meilleurs hyperparamètres pour prédire sur les données de test
+    y_pred = random_search.best_estimator_.predict(X_test)
+
+    # Calculer et afficher l'accuracy
     accuracy = accuracy_score(y_test, y_pred)
-    
-    # Extraire l'importance des caractéristiques
-    if hasattr(model, 'feature_importances_'):
-        feature_importances = model.feature_importances_
-    elif hasattr(model, 'coef_'):
-        feature_importances = model.coef_
-    else:
-        feature_importances = None
-    
-    # Stocker les résultats
-    results[model_name] = {'model': model, 'accuracy': accuracy, 'feature_importances': feature_importances}
-    
-    # Stocker les importances dans le DataFrame
-    importances_df[model_name] = feature_importances
+    st.write("Accuracy sur les données de test :")
+    st.write(accuracy)
 
-# Créer un DataFrame à partir des résultats
-results_df = pd.DataFrame(results).T
-results_df = results_df[['accuracy']]  # Sélectionner les colonnes de métriques
+    # Afficher la matrice de confusion
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    st.write("Matrice de Confusion :")
+    st.write(conf_matrix)
 
-# Afficher le tableau avec les métriques
-st.write("Comparaison des modèles (Métriques de régression) :")
-st.table(results_df)
-
-# Afficher les importances des caractéristiques avec un graphique à barres
-plt.style.use('dark_background')  # Utiliser un fond noir
-importances_df.plot(kind='bar', figsize=(10, 6), title='Importance des Caractéristiques dans Chaque Modèle', xlabel='Caractéristiques', ylabel='Importance', colormap='viridis')
-
-# Ajuster les paramètres du graphique pour améliorer la lisibilité
-plt.xticks(rotation=45, ha='right')  # Rotation des étiquettes sur l'axe x
-plt.tight_layout()  # Ajustement automatique de la mise en page pour éviter les coupures
-plt.legend(loc='upper right')  # Ajouter une légende
-
-# Afficher le graphique
-st.pyplot(plt)
-
-
-## CHOSIR XGBOOST ET OPTIMISER LES HYPERPARAMETRE POUR AVOIR UN MODELE TROP BIEN
-# Séparer les features et la variable cible
-X = data[['age', 'region2', 'catr', 'trajet']]
-y = data['grav']
-
-# Diviser les données en ensembles d'entraînement et de test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Définir les hyperparamètres à optimiser
-param_dist = {
-    'learning_rate': [0.01, 0.1, 0.2],
-    'n_estimators': [50, 100, 200],
-    'max_depth': [3, 5, 7],
-    'subsample': [0.8, 0.9, 1.0],
-    'colsample_bytree': [0.8, 0.9, 1.0],
-    'gamma': [0, 1, 2],
-    'min_child_weight': [1, 2, 3],
-    'lambda': [0, 1, 2],
-    'alpha': [0, 1, 2]
-}
-
-# Créer le modèle XGBoost
-xgb = XGBClassifier()
-
-# Effectuer une recherche aléatoire des hyperparamètres
-random_search = RandomizedSearchCV(xgb, param_distributions=param_dist, n_iter=10, scoring='accuracy', cv=3, random_state=42)
-random_search.fit(X_train, y_train)
-
-# Afficher les meilleurs hyperparamètres
-best_params = random_search.best_params_
-print("Meilleurs hyperparamètres :", best_params)
-
-# Utiliser le modèle avec les meilleurs hyperparamètres pour prédire sur les données de test
-y_pred = random_search.best_estimator_.predict(X_test)
-
-# Calculer et afficher l'accuracy
-accuracy = accuracy_score(y_test, y_pred)
-st.write("Accuracy sur les données de test :")
-st.write(accuracy)
-
-# Séparer les features et la variable cible
-X = data[["age", "region2", "catr", "trajet"]]
-y = data['grav']
-
-# Imputer les valeurs manquantes
-imputer = SimpleImputer(strategy='most_frequent')
-X_imputed = pd.DataFrame(imputer.fit_transform(X), columns=X.columns)
-
-# Diviser les données en ensembles d'entraînement et de test
-X_train, X_test, y_train, y_test = train_test_split(X_imputed, y, test_size=0.2, random_state=42)
-
-# Utiliser le modèle XGBoost avec les meilleurs hyperparamètres pour prédire sur l'ensemble initial de features
-y_pred_best = random_search.best_estimator_.predict(X_test)
-
-# Calculer la matrice de confusion
-conf_matrix = confusion_matrix(y_test, y_pred_best, normalize='true')
-
-# Afficher la matrice de confusion sous forme de heatmap
-plt.figure(figsize=(8, 6))
-sns.heatmap(conf_matrix, annot=True, fmt=".2f", cmap="Blues", xticklabels=['Indemne', 'Blesse_hospi', "hospi_leger", "Tue"], yticklabels=['Indemne', 'Blesse_hospi', "hospi_leger", "Tue"])
-plt.title('Matrice de Confusion - Modèle XGBoost')
-plt.xlabel('Vrai')
-plt.ylabel('Prédit')
-plt.tight_layout()
-
-# Afficher le graphique
-st.pyplot(plt)
+xgboost_merged_classes()
 
 # Liste des régions françaises métropolitaines
 regions_francaises = [
